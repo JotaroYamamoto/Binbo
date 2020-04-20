d68ba2b88920582503dd72ee32b726794db4eb74
JotaroYamamoto <kensin2131@gmail.com>
2020-04-20 21:05:42 +0900

    フロント実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,91 +1,77 @@
 @extends('layouts.binbo')
 
     @section('content')
-    <div class="main">
-      <div class="main--contents">
-        <article class="article">
-          <a href="#" class="articleIn">
-            <div class="article__img">
-              <img src="https://umablo.info/wp-content/uploads/2020/03/DSC_0129-360x230.png" alt="">
-            </div>
-            <div class="article__bottom">
-              <div class="post-title">
-                マスタードカルパッチョ
-              </div>
-              <div class="post-price">
-              原価：100円
-              </div>
-            </div>
-          </a>
-        </article>
-        <article class="article">
-          <a href="#" class="articleIn">
-            <div class="article__img">
-              <img src="https://umablo.info/wp-content/uploads/2020/03/DSC_0129-360x230.png" alt="">
-            </div>
-            <div class="article__bottom">
-              <div class="post-title">
-                マスタードカルパッチョ
-              </div>
-              <div class="post-price">
-                原価：100円
-              </div>
-            </div>
-          </a>
-        </article>
-        <article class="article">
-          <a href="#" class="articleIn">
-            <div class="article__img">
-              <img src="https://umablo.info/wp-content/uploads/2020/03/DSC_0129-360x230.png" alt="">
-            </div>
-            <div class="article__bottom">
-              <div class="post-title">
-                マスタードカルパッチョ
-              </div>
-              <div class="post-price">
-              原価：100円
-              </div>
-            </div>
-          </a>
-            </div>
-          </a>
-        </article>
-        <div class="main-contents">
-          <article class="article">
-            <a href="#" class="articleIn">
-              <div class="article__img">
-                <img src="https://umablo.info/wp-content/uploads/2020/03/DSC_0129-360x230.png" alt="">
-              </div>
-              <div class="article__bottom">
-                <div class="post-title">
-                  マスタードカルパッチョ
-                </div>
-                <div class="post-price">
-                原価：100円
-                </div>
-              </div>
-            </a>
-              </div>
-            </a>
-          </article>
-          <article class="article">
-            <a href="#" class="articleIn">
-              <div class="article__img">
-                <img src="https://umablo.info/wp-content/uploads/2020/03/DSC_0129-360x230.png" alt="">
-              </div>
-              <div class="article__bottom">
-                <div class="post-title">
-                  マスタードカルパッチョ
-                </div>
-                <div class="post-price">
-                原価：100円
-                </div>
-              </div>
-            </a>
-              </div>
-            </a>
-          </article>
-        </div>
+    <div class="under-header">
+      <div class="top-img">
       </div>
     </div>
+    <div class="main">
+      <div class="main--contents">
+          <a href="#" class="article">
+            <div class="article__img">
+              <img src="https://umablo.info/wp-content/uploads/2020/03/DSC_0129-360x230.png" alt="">
+            </div>
+            <div class="article__bottom">
+              <div class="post-title">
+                マスタードカルパッチョ
+              </div>
+              <div class="post-price">
+              原価：100円
+              </div>
+            </div>
+          </a>
+          <a href="#" class="article">
+            <div class="article__img">
+              <img src="https://umablo.info/wp-content/uploads/2020/03/DSC_0129-360x230.png" alt="">
+            </div>
+            <div class="article__bottom">
+              <div class="post-title">
+                マスタードカルパッチョ
+              </div>
+              <div class="post-price">
+              原価：100円
+              </div>
+            </div>
+          </a>
+          <a href="#" class="article">
+            <div class="article__img">
+              <img src="https://umablo.info/wp-content/uploads/2020/03/DSC_0129-360x230.png" alt="">
+            </div>
+            <div class="article__bottom">
+              <div class="post-title">
+                マスタードカルパッチョ
+              </div>
+              <div class="post-price">
+              原価：100円
+              </div>
+            </div>
+          </a>
+          <a href="#" class="article">
+            <div class="article__img">
+              <img src="https://umablo.info/wp-content/uploads/2020/03/DSC_0129-360x230.png" alt="">
+            </div>
+            <div class="article__bottom">
+              <div class="post-title">
+                マスタードカルパッチョ
+              </div>
+              <div class="post-price">
+              原価：100円
+              </div>
+            </div>
+          </a>
+          <a href="#" class="article">
+            <div class="article__img">
+              <img src="https://d2l930y2yx77uc.cloudfront.net/production/uploads/images/17360471/picture_pc_c5dfa9fdd29d22629b0d4bb9937bef21.jpg">
+            </div>
+            <div class="article__bottom">
+              <div class="post-title">
+                マスタードカルパッチョ
+              </div>
+              <div class="post-price">
+              原価：100円
+              </div>
+            </div>
+          </a>
+  </div>
     @endsection
+
